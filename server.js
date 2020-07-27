0f55fbf42b69a3599034ffb7523544d241cc111e
AnujKBagerwal <anuj.bagerwal@gmail.com>
2020-07-27 19:24:54 +0530

    GraphQL

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+const PORT = 8000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
